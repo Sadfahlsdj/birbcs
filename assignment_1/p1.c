882ad1dd86d4bbdc5ac3f07a65ff0a0622bdfd74
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2025-05-16 13:00:17 -0700

    ACTUALLY PUTTING THESE ON THE RIGHT REPO THIS TIME

--- assignment_1/p1.c
+++ assignment_1/p1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
-    printf("hello world from process %d\n", getpid());
+    for(int i = 1; i < 101; i++){
+        char word[9] = "        ";
+        i % 15 == 0 ? strcpy(word, "fizzbuzz") : i % 5 == 0 ? strcpy(word, "buzz") : i % 3 == 0 ? strcpy(word, "fizz") : strcpy(word, "");
+        printf("%d %s\n", i, word);
+    }
 }
--- assignment_1/p2.c
+++ assignment_1/p2.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+void rev(char *start, char *end) {
+    /*
+     * takes in start & end pointers, reverses everything between them (inclusive)
+     * intended to be used on subsets of a string which are only chars
+     */
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
 int main() {
     char buf[1024];
-    int count = 0;
     FILE *in = fopen("README.md", "r");
     FILE *out = fopen("out.md", "w");
     while (fgets(buf, sizeof(buf), in)) {
-        ++count;
-        for (int i = 0; buf[i] != '\0'; ++i) {
-            fprintf(out, "%d ", buf[i]);
+        size_t len = strlen(buf);
+        char *start = NULL;
+
+        for (size_t i = 0; i < len; i++) {
+            if (isalpha(buf[i])) {
+                if (start == NULL) {
+                    start = &buf[i]; // start a word
+                }
+            }
+            else {
+                if (start != NULL) {
+                    rev(start, &buf[i - 1]); // reverse alphabetic section
+                    start = NULL; // reset
+                }
+            }
         }
+        if (start != NULL) { // in case a word ends the line
+            rev(start, &buf[len-1]);
+        }
+        printf("%s", buf);
     }
-    printf("line count: %d\n", count);
     fclose(in);
     fclose(out);
 }
